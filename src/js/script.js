8ab205bc6ff62d2c2d8aad1a5a4cc729ab3d3674
Bruno Nunes <123209025+Bruno-Nunes17@users.noreply.github.com>
2023-02-23 19:34:38 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,61 +4,101 @@ const jogoVelha = [
     ['', '', '']
 ];
 let jogador = 'O';
+let contadorDeJogadas = 0;
+let posicoesVitoriosas = [];
 
 window.onload = function () {
+    const botaoReiniciar = document.querySelector("a");
     const listElement = document.querySelectorAll("#jogo > div");
-    function checarVitoria(){
-        for(let i = 0; i<3; i++){
-            if(jogoVelha[0][i] != '' && jogoVelha[0][i] == jogoVelha[1][i] && jogoVelha[1][i] == jogoVelha[2][i]){
+    const vezJogador = document.querySelector("h1");
+    let fimJogo = false;
+    let jogadorDois = 0;
+    let jogadorUm = 0;
+
+    function checarVitoria() {
+        for (let i = 0; i < 3; i++) {
+            if (jogoVelha[0][i] != '' && jogoVelha[0][i] == jogoVelha[1][i] && jogoVelha[1][i] == jogoVelha[2][i]) {
+                posicoesVitoriosas.push(i + 1)
+                posicoesVitoriosas.push(i + 4)
+                posicoesVitoriosas.push(i + 7)
                 return jogoVelha[0][i];
             }
-            if(jogoVelha[i][0] != '' && jogoVelha[i][0] == jogoVelha[i][1] && jogoVelha[i][1] == jogoVelha[i][2]){
+            if (jogoVelha[i][0] != '' && jogoVelha[i][0] == jogoVelha[i][1] && jogoVelha[i][1] == jogoVelha[i][2]) {
+                posicoesVitoriosas.push((i * 3) + 1)
+                posicoesVitoriosas.push((i * 3) + 2)
+                posicoesVitoriosas.push((i * 3) + 3)
                 return jogoVelha[i][0];
             }
-            
+
         }
-        if(jogoVelha[0][0] != '' 
-            && jogoVelha[0][0] == jogoVelha[1][1] 
-            && jogoVelha[1][1] == jogoVelha[2][2]){
-                return jogoVelha[0][0];
-            }
-            if(jogoVelha[2][0] != '' 
-            && jogoVelha[2][0] == jogoVelha[1][1] 
-            && jogoVelha[1][1] == jogoVelha[0][2]){
-                return jogoVelha[2][0];
-            }
+        if (jogoVelha[0][0] != ''
+            && jogoVelha[0][0] == jogoVelha[1][1]
+            && jogoVelha[1][1] == jogoVelha[2][2]) {
+            posicoesVitoriosas = [1, 5, 9]
+            return jogoVelha[0][0];
+        }
+        if (jogoVelha[2][0] != ''
+            && jogoVelha[2][0] == jogoVelha[1][1]
+            && jogoVelha[1][1] == jogoVelha[0][2]) {
+            posicoesVitoriosas = [7, 5, 3]
+            return jogoVelha[2][0];
+        }
         return '';
     }
     function clickElement(elementoClicado) {
-        const item = elementoClicado.target;
-        const numeroItem = item.dataset.item;
-        const linha = Math.floor((numeroItem - 1) / 3);
-        const coluna = (numeroItem - 1) % 3;
+        if (!fimJogo) {
+            const item = elementoClicado.target;
+            const numeroItem = item.dataset.item;
+            const linha = Math.floor((numeroItem - 1) / 3);
+            const coluna = (numeroItem - 1) % 3;
 
-        if (item.innerText == '') {
-            jogoVelha[linha][coluna] = jogador;
-            item.innerText = jogador;
-            const jogadorVencedor = checarVitoria();
+            if (item.textContent == '') {
+                contadorDeJogadas++;
+                jogoVelha[linha][coluna] = jogador;
+                item.textContent = jogador;
+                const jogadorVencedor = checarVitoria();
 
-            if(jogadorVencedor != ''){
+                if (jogadorVencedor != '') {
+                    if (jogadorVencedor == 'X') {
+                        fimJogo = true;
+                        for (let i = 0; i < posicoesVitoriosas.length; i++) {
+                            const atributoElemento = `[data-item="${posicoesVitoriosas[i]}"]`;
+                            document.querySelector(atributoElemento).style.color = 'red';
+                        }
+                        vezJogador.textContent = `O jogador vencedor é "${jogadorVencedor}"!`
+                        botaoReiniciar.classList.add("aberto")
+                        return
+                    }
+                    else {
+                        fimJogo = true;
+                        for (let i = 0; i < posicoesVitoriosas.length; i++) {
+                            const atributoElemento = `[data-item="${posicoesVitoriosas[i]}"]`;
+                            document.querySelector(atributoElemento).style.color = 'red';
+                        }
+                        vezJogador.textContent = `O jogador vencedor é "${jogadorVencedor}"!`
+                        botaoReiniciar.classList.add("aberto")
+                        return
+                    }
+                }
+                else if (contadorDeJogadas == 9) {
+                    fimJogo = true;
+                    vezJogador.textContent = `Deu velha!`
+                    botaoReiniciar.classList.add("aberto")
+                    return
+                }
 
-                alert(`O campeão foi o jogador ${jogadorVencedor}`)
-                return
+                if (jogador == "O") {
+                    jogador = "X"
+                }
+                else {
+                    jogador = "O"
+                }
+                vezJogador.textContent = `O jogador da vez é "${jogador}"`
             }
-
-            if (jogador == "O") {
-                jogador = "X"
-            }
-            else {
-                jogador = "O"
-            }
-
         }
-
     }
 
     for (let i = 0; i < listElement.length; i++) {
         listElement[i].addEventListener('click', clickElement)
     }
-
 };
